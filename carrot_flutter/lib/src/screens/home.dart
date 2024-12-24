56ee66a3be00fe26b639a17af674573a46434d42
seungil oh <seungil.oh@rwth-aachen.de>
2024-12-24 15:05:03 +0900

    풀스택 개발 3장-3 완료

--- carrot_flutter/lib/src/screens/home.dart
+++ carrot_flutter/lib/src/screens/home.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
 final List<BottomNavigationBarItem> myTabs = <BottomNavigationBarItem>[
   BottomNavigationBarItem(
     icon: Icon(Icons.home),
