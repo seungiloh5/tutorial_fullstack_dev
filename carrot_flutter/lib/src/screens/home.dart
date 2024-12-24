855522314a92173352674856f21d8d10256db04e
seungil oh <seungil.oh@rwth-aachen.de>
2024-12-24 15:04:20 +0900

    풀스택 개발 3장-3.2 하단 탭 네비게이션

--- carrot_flutter/lib/src/screens/home.dart
+++ carrot_flutter/lib/src/screens/home.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
-final List<Tab> myTabs = <Tab>[
-  Tab(icon: Icon(Icons.home), text: '홈'),
-  Tab(icon: Icon(Icons.feed), text: '동네'),
-  Tab(icon: Icon(Icons.chat_bubble_outline_rounded), text: '채팅'),
-  Tab(icon: Icon(Icons.person_outline), text: '마이'),
+import 'package:flutter/material.dart';
+
+final List<BottomNavigationBarItem> myTabs = <BottomNavigationBarItem>[
+  BottomNavigationBarItem(
+    icon: Icon(Icons.home),
+    label: '홈',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.feed),
+    label: '동네',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.chat_bubble_outline_rounded),
+    label: '채팅',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.person_outline),
+    label: '마이',
+  ),
 ];
 
 final List<Widget> myTabItems = [
@@ -14,32 +28,50 @@ final List<Widget> myTabItems = [
   Center(child: Text('마이')),
 ];
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: myTabs.length,
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          title: Text('내 동네'),
-          bottom: TabBar(tabs: myTabs),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.search),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.notifications_none_rounded),
-            ),
-          ],
-        ),
-        body: TabBarView(
-          children: myTabItems,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text('내 동네'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.notifications_none_rounded),
+          ),
+        ],
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: myTabItems,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        items: myTabs,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
