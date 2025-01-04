53e9e412b89231ce085f995e0cb8a70ad87a8124
seungil oh <seungil.oh@rwth-aachen.de>
2025-01-04 16:27:39 +0900

    풀스택 개발 6장-3.2 페이지 화면

--- carrot_flutter/lib/src/screens/my/mypage.dart
+++ carrot_flutter/lib/src/screens/my/mypage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../widgets/listitems/user_mypage.dart';
 import '../../models/user_model.dart';
+import '../../screens/my/webpage.dart';
 
 class MyPage extends StatelessWidget {
   const MyPage({super.key});
@@ -35,13 +36,13 @@ class MyPage extends StatelessWidget {
             ListTile(
               title: const Text('이용약관'),
               onTap: () {
-                // Get.to(() => const WebPage('이용약관', '/page/terms'));
+                Get.to(() => const WebPage('이용약관', '/page/terms'));
               },
             ),
             ListTile(
               title: const Text('개인정보 처리방침'),
               onTap: () {
-                // Get.to(() => const WebPage('개인정보 처리방침', '/page/policy'));
+                Get.to(() => const WebPage('개인정보 처리방침', '/page/policy'));
               },
             ),
           ],
---
+++ carrot_flutter/lib/src/screens/my/webpage.dart
@@ -0,0 +1,19 @@
+// import 'package:carrot_flutter/src/shared/global.dart';
+import 'package:flutter/material.dart';
+
+class WebPage extends StatelessWidget {
+  final String title;
+  final String url;
+  const WebPage(this.title, this.url, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      // body: Center(child: Text("페이지 로드: ${Global.baseUrl}$url")),
+      body: Center(child: Text('웹 페이지')),
+    );
+  }
+}
