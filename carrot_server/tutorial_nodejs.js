b2df77a9142982467c32169d5f22aafd215ee4a0
seungil oh <seungil.oh@rwth-aachen.de>
2025-01-04 22:51:44 +0900

    풀스택 개발 7장-2.3 타입 변환과 비교

--- carrot_server/tutorial_nodejs.js
+++ carrot_server/tutorial_nodejs.js
@@ -82,3 +82,22 @@ console.log(notDefined);
 let sym1 = Symbol('symbol1');
 let sym2 = Symbol('symbol2');
 console.log(sym1 === sym2); // false
+
+// implicit type conversion ------------------------------------------------------
+let result = 3 + '30';
+console.log(result); // '330'
+
+// explicit type conversion ------------------------------------------------------
+let num = Number('5');
+console.log(num); // 5
+
+// equality ------------------------------------------------------
+console.log(3 == '3'); // true, 동등 연산자
+console.log(3 === '3'); // false, 일치 연산자
+
+// Truthy and Falsy ------------------------------------------------------
+if ('') {
+    console.log('Truthy');
+} else {
+    console.log('Falsy');
+}
