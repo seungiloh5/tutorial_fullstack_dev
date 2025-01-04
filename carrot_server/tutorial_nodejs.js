e6b57cb0cd5d9b0a6c7c07e3e2f4f9ef02f0f066
seungil oh <seungil.oh@rwth-aachen.de>
2025-01-04 22:48:39 +0900

    풀스택 개발 7장-2.2 기본 자료형과 객체

--- carrot_server/tutorial_nodejs.js
+++ carrot_server/tutorial_nodejs.js
@@ -49,3 +49,36 @@ function tag(strings, value1){
 const var1 = "world";
 console.log(tag`Hello, ${var1}!`);
 
+// numbers -----------------------------------------------------------
+let integer = 42;
+let float = 42.195;
+let special = NaN;
+
+// strings -----------------------------------------------------------
+let singleQouted = 'Hello, world!';
+let doubleQouted = "Hello, world!";
+let backtick = `Hello, world!`;
+
+// booleans -----------------------------------------------------------
+let isTrue = true;
+let isFalse = false;
+
+// arrays -----------------------------------------------------------
+let array = [1, 'two', true, 4];
+console.log(array[1]);
+
+// objects -----------------------------------------------------------
+let object = {
+    key1: 'value1',
+    key2: 'value2',
+    key3: 'value3'
+};
+console.log(object.key2);
+
+// null, undefined, symbols ------------------------------------------------------
+let empty = null;
+let notDefined = undefined;
+console.log(notDefined);
+let sym1 = Symbol('symbol1');
+let sym2 = Symbol('symbol2');
+console.log(sym1 === sym2); // false
