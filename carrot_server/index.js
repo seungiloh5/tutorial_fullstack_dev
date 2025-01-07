6cace9c513aad2043586ebcf647b4044384d46a3
seungil oh <seungil.oh@rwth-aachen.de>
2025-01-07 22:04:07 +0900

    풀스택 개발 8장-1.1 프레임워크 소개

--- carrot_server/index.js
+++ carrot_server/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const express = require('express');
+const app = express();
+const port = process.env.PORT||3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+}); 
+
+app.get('/about', (req, res) => {
+    res.send('About page');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
