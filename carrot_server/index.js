60daf02493d61833d5f152a41416b198d4dc1b51
seungil oh <seungil.oh@rwth-aachen.de>
2025-01-08 21:18:01 +0900

    풀스택 개발 8장-1.2 라우팅과 HTTP 요청/응답 처리

--- carrot_server/index.js
+++ carrot_server/index.js
@@ -3,13 +3,45 @@ const app = express();
 const port = process.env.PORT||3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('홈페이지');
 }); 
 
-app.get('/about', (req, res) => {
-    res.send('About page');
+app.get('/page/policy', (req, res) => {
+    res.send('이용 약관');
+});
+
+app.get('/page/terms', (req, res) => {
+    res.send('개인정보 처리방침');
+});
+
+app.get('/api/user/my', (req, res) => {
+    res.send('내 정보');
+});
+
+app.put('/api/user/my', (req, res) => {
+    res.send('내 정보 수정');
+});
+
+app.get('/api/feed', (req, res) => {
+    res.send('피드 목록');
+});
+
+app.post('/api/feed', (req, res) => {
+    res.send('피드 생성');
+});
+
+app.get('/api/feed/:feed', (req, res) => {
+    res.send('피드 상세');
+});
+
+app.put('/api/feed/:feed', (req, res) => {
+    res.send('피드 수정');
+});
+
+app.delete('/api/feed/:feed', (req, res) => {
+    res.send('피드 삭제');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`웹서버 구동 ... ${port}`);
 });
