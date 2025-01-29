431ede8f63a1079fdfff7fb01aec695f676236d6
seungil oh <seungil.oh@rwth-aachen.de>
2025-01-29 22:15:07 +0900

    풀스택 개발 11-4 WebView Flutter 구현 (버그 존재)

--- carrot_flutter/lib/src/screens/my/webpage.dart
+++ carrot_flutter/lib/src/screens/my/webpage.dart
@@ -1,19 +1,35 @@
-// import 'package:carrot_flutter/src/shared/global.dart';
+import 'package:carrot_flutter/src/shared/global.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class WebPage extends StatelessWidget {
+class WebPage extends StatefulWidget {
   final String title;
   final String url;
   const WebPage(this.title, this.url, {super.key});
 
   @override
+  State<WebPage> createState() => _WebPageState();
+}
+
+class _WebPageState extends State<WebPage> {
+  late WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse("${Global.baseUrl}${widget.url}"),
+      );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      // body: Center(child: Text("페이지 로드: ${Global.baseUrl}$url")),
-      body: Center(child: Text('웹 페이지')),
+      body: WebViewWidget(controller: controller), // 백엔드 부분 구현 필요
     );
   }
 }
