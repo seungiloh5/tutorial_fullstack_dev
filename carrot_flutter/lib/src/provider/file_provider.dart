5d645721cb3ad7d333a99a976e3a0e2e48149622
seungil oh <seungil.oh@rwth-aachen.de>
2025-03-10 21:57:31 +0900

    simulator 안켜지던 문제 디버깅

--- carrot_flutter/lib/src/provider/file_provider.dart
+++ carrot_flutter/lib/src/provider/file_provider.dart
@@ -1,12 +1,13 @@
 import 'package:get/get.dart';
-
 import 'provider.dart';
 
 class FileProvider extends Provider {
   imageUpload(String name, String path) async {
     final form = FormData({'file': MultipartFile(path, filename: name)});
+    print(form);
 
-    Response response = await post('/file', form);
+    Response response = await post('/file/upload', form);
+    print(response.body);
     return response.body;
   }
 }
